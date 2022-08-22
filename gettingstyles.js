3ff279bae9a3fd890c987940dfd94810364fde66
dinesh <bogidany3305@gmail.com>
2022-08-22 19:11:48 +0530

    gettingstyles

---
+++ gettingstyles.js
@@ -0,0 +1,4 @@
+document.getElementById("diny").style.color="red";
+document.getElementById("diny").style.fontSize="20px";
+document.querySelector('p').style.fontStyle="italic";
+document.getElementById("diny").style.backgroundColor="navy";
